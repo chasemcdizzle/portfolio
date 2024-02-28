3204c115f3d21c9abf4707c48b6e547209e1ea12
Nicholas Chase <nickscottchase@proton.me>
2024-02-28 14:52:34 -0800

    finished all mobile stuff. remaining action items are: 	get rid of big gap between end of projects and "contact me" section 	get real portfolio pictures in 	get ddos protection?

--- src/components/main/Skills.tsx
+++ src/components/main/Skills.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 
 const Skills = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  //   useEffect(() => {
+  //     if (isMobile) {
+  //       collapse();
+  //     } else {
+  //       resetWidth();
+  //     }
+  //   }, [isMobile]);
+
   return (
     <>
       <h1
@@ -10,21 +21,57 @@ const Skills = () => {
       >
         Skills
       </h1>
-      <div className=" w-full dark:bg-background bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-white [mask-image:radial-gradient(ellipse_at_center,transparent,black)]"></div>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-          <div className="flex flex-row items-center justify-center mb-10 w-full">
-            <AnimatedTooltip items={skills} />
-          </div>
-        </p>
-        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
-          <div className="flex flex-row items-center justify-center mb-10 w-full">
-            <AnimatedTooltip items={skills2} />
-          </div>
-        </p>
-        <br />
-      </div>
+      {!isMobile ? (
+        <div className=" w-full dark:bg-background bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-white [mask-image:radial-gradient(ellipse_at_center,transparent,black)]"></div>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            <div className="flex flex-row items-center justify-center mb-10 w-full">
+              <AnimatedTooltip items={skills} />
+            </div>
+          </p>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
+            <div className="flex flex-row items-center justify-center mb-10 w-full">
+              <AnimatedTooltip items={skills2} />
+            </div>
+          </p>
+          <br />
+        </div>
+      ) : (
+        <div className=" w-full dark:bg-background bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-white [mask-image:radial-gradient(ellipse_at_center,transparent,black)]"></div>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            <div className="flex flex-row items-center justify-center mb-10 w-full">
+              <AnimatedTooltip
+                items={skills.slice(0, Math.floor(skills.length / 2) + 1)}
+              />
+            </div>
+          </p>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+            <div className="flex flex-row items-center justify-center mb-10 w-full">
+              <AnimatedTooltip
+                items={skills.slice(Math.floor(skills.length / 2) + 1)}
+              />
+            </div>
+          </p>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
+            <div className="flex flex-row items-center justify-center mb-10 w-full">
+              <AnimatedTooltip
+                items={skills2.slice(0, Math.floor(skills2.length / 2))}
+              />
+            </div>
+          </p>
+          <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
+            <div className="flex flex-row items-center justify-center mb-10 w-full">
+              <AnimatedTooltip
+                items={skills2.slice(Math.floor(skills2.length / 2))}
+              />
+            </div>
+          </p>
+          <br />
+        </div>
+      )}
     </>
   );
 };
@@ -125,11 +172,6 @@ const skills = [
     name: "Vercel",
     image: "/vercel.svg",
   },
-  {
-    id: 20,
-    name: "Webhook",
-    image: "/webhook.svg",
-  },
 ];
 
 const skills2 = [
@@ -217,6 +259,11 @@ const skills2 = [
     id: 17,
     name: "C++",
     image: "/c++.png",
+  },
+  {
+    id: 18,
+    name: "Webhook",
+    image: "/webhook.svg",
   },
 ];
 
