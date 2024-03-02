536dde1e0193b930792929fb747fb886126c1611
Nicholas Chase <nickscottchase@proton.me>
2024-03-02 13:40:20 -0800

    got rid of meteors over work experience (it was distracting)
    now have a moving border around it
    (less distracting but still flashy)

--- src/components/main/Experience.tsx
+++ src/components/main/Experience.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SparklesCore } from "@/components/ui/sparkles";
 import { Meteors } from "@/components/ui/meteors";
+import { MovingBorder, Button } from "../ui/moving-border";
 
 const Experience = () => {
   return (
@@ -13,7 +14,22 @@ const Experience = () => {
       <div className="flex flex-col md:flex-row gap-y-6 h-full items-center align-middle justify-center px-6 py-1 gap-x-6">
         <div className="h-full relative max-w-xs min-w-[260px] w-[260px]">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-          <div className="relative shadow-xl bg-secondary border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <Button
+            borderRadius="1.75rem"
+            className="relative shadow-xl bg-secondary border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start"
+          >
+            {/* <div
+              className="absolute inset-0"
+              style={{ borderRadius: `calc(${1} * 0.96)` }}
+            >
+              <MovingBorder rx="30%" ry="30%">
+                <div
+                  className={
+                    "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]"
+                  }
+                />
+              </MovingBorder>
+            </div> */}
             <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -43,12 +59,15 @@ const Experience = () => {
             </div>
 
             {/* Meaty part - Meteor effect */}
-            <Meteors number={20} />
-          </div>
+            {/* <Meteors number={20} /> */}
+          </Button>
         </div>
         <div className="h-full relative max-w-xs min-w-[260px] w-[260px]">
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-          <div className="relative shadow-xl bg-secondary border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <Button
+            borderRadius="1.75rem"
+            className="relative shadow-xl bg-secondary border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start"
+          >
             <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -78,8 +97,8 @@ const Experience = () => {
             </div>
 
             {/* Meaty part - Meteor effect */}
-            <Meteors number={20} />
-          </div>
+            {/* <Meteors number={20} /> */}
+          </Button>
         </div>
       </div>
     </>
